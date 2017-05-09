0074161f2aa801e014dbc1af788bd0994b5d76b9
jules _ <jules _>
2017-05-09 16:24:39 -0700

    Add testheap namespace.

--- src/testheap.cpp
+++ src/testheap.cpp
@@ -8,29 +8,33 @@
 #include <cstring>
 using namespace std;
 
-void assert_it(bool test) {
-	try {
-		if ( !test ) {
-			throw "Test failed!";
+namespace testheap
+{
+	void assert_it(bool test) {
+		try {
+			if ( !test ) {
+				throw "Test failed!";
+			}
+		}
+		catch (const char* msg) {
+			cout << msg << endl;
 		}
 	}
-	catch (const char* msg) {
-		cout << msg << endl;
+
+	void assert_equal(int a, int b) {
+		assert_it(a == b);
 	}
-}
 
-void assert_equal(int a, int b) {
-	assert_it(a == b);
-}
-
-void assert_equal(char *a, char *b) {
-	assert_it(!(bool)strcmp(a,b));
+	void assert_equal(char *a, char *b) {
+		assert_it(!(bool)strcmp(a,b));
+	}
 }
 
 int main() {
+	using namespace testheap;
+
 	char a[] = "String a";
-	char b[] = "String a";
-	cout << (bool)strcmp(a,b) << endl;
+	char b[] = "String b";
 	assert_equal(a, b);
 	return 0;
 }
